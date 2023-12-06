860e50e1e2b3959e3e0fa468c8047975c2ccd34d
YOOSUNAH <tjsdkyd4027@gmail.com>
2023-12-06 17:24:44 +0900

    MySQL 데이터 베이스 생성, 스프링 부트에서 MySQL DB 접속정보 수정

--- src/main/java/toy/ojm/client/dto/SearchLocalRes.java
+++ src/main/java/toy/ojm/client/dto/SearchLocalRes.java
@@ -7,6 +7,8 @@ import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
--- src/main/java/toy/ojm/repository/MemberRepository.java
+++ src/main/java/toy/ojm/repository/MemberRepository.java
@@ -1,4 +1,14 @@
 package toy.ojm.repository;
 
-public interface MemberRepository {
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import toy.ojm.entity.MemberEntity;
+
+@Repository
+public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
+
+
+
+
 }
