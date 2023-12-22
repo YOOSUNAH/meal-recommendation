c0177e995507e45501f2f12f16c41201d0a8b114
YOOSUNAH <tjsdkyd4027@gmail.com>
2023-12-22 18:01:58 +0900

    사용자에게 보여줄 결과가 있을 경우, 없을 경우를 나눠서 html 작성, pageController 같이 수정

--- src/main/java/toy/ojm/controller/PageController.java
+++ src/main/java/toy/ojm/controller/PageController.java
@@ -1,7 +1,9 @@
 package toy.ojm.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import toy.ojm.controller.dto.MealRecommendationResponse;
 
 //@Slf4j
 @Controller
@@ -13,10 +15,16 @@ public class PageController {
     }
 
     @GetMapping("/result-page")
-    public String goResult() {
-        return "result";
+    public String goResult(Model model, MealRecommendationResponse recommendationResponse) {
+        if (recommendationResponse.getList() != null && !recommendationResponse.getList().isEmpty()) {
+            // 결과가 있는 경우 - result.html 리턴
+            model.addAttribute("recommendedItems", recommendationResponse.getList());
+            return "result";
+        } else {
+            // 결과가 없는 경우 - noresult.html 리턴
+            return "noresult";
+        }
     }
-
 
 
 }
