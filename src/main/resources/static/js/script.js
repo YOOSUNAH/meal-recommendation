3777d9f73a4b1657984fa4dc598342d37f97785c
YOOSUNAH <tjsdkyd4027@gmail.com>
2023-12-27 14:34:01 +0900

    버튼 크기 수정
    버튼의 크기가 너무 작아 크기를 수정했다.
    html과 css파일을 수정했다.
    html에는 class이름을 지정하고 css 파일에 실제 크기 및 디자인 부분을 작성했다.

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -2,22 +2,18 @@ document.addEventListener('DOMContentLoaded', function () {
     const swiper = new Swiper('.swiper-container', {
         direction: 'horizontal',
         loop: false,
-
         slidesPerView: 1,
         spaceBetween: 10, // Optional: Set space between slides to 0
         centeredSlides: true,
-
         pagination: {
             el: '.swiper-pagination',
             clickable: true
         },
-
         // Navigation arrows
         navigation: {
             nextEl: '.swiper-button-next-custom',
             prevEl: '.swiper-button-prev',
         },
-
         // And if we need scrollbar
         scrollbar: {
             el: '.swiper-scrollbar',
@@ -27,25 +23,46 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 function selectAllCategory() {
-    // '전체 선택하기' 체크박스의 상태를 가져옵니다.
-    let selectAll = document.getElementById("All").checked;
+    const koreanCheckbox = document.getElementById("Korean");
+    const japaneseCheckbox = document.getElementById("Japanese");
+    const chineseCheckbox = document.getElementById("Chinese");
+    const westernCheckbox = document.getElementById("Western");
+    const selectAllCheckbox = document.getElementById("All");
 
-    // 모든 체크박스의 상태를 '전체 선택하기' 체크박스의 상태와 동일하게 만듭니다.
-    document.getElementById("Korean").checked = selectAll;
-    document.getElementById("Japanese").checked = selectAll;
-    document.getElementById("Chinese").checked = selectAll;
-    document.getElementById("Western").checked = selectAll;
+    const checkboxes = [koreanCheckbox, japaneseCheckbox, chineseCheckbox, westernCheckbox];
+
+    // 각 카테고리 체크박스의 변화를 감지하여 '전체 선택하기' 상태를 조절합니다.
+    checkboxes.forEach(checkbox => {
+        checkbox.addEventListener('change', () => {
+            if (!checkbox.checked) {
+                selectAllCheckbox.checked = false;
+            } else {
+                const allChecked = checkboxes.every(cb => cb.checked);
+                selectAllCheckbox.checked = allChecked;
+            }
+        });
+    });
+
+    // '전체 선택하기' 체크박스의 상태에 따라 모든 카테고리 체크박스를 제어합니다.
+    selectAllCheckbox.addEventListener('change', () => {
+        checkboxes.forEach(checkbox => {
+            checkbox.checked = selectAllCheckbox.checked;
+        });
+    });
 }
 
-function recommend() {
 
+function recommend() {
     navigator.geolocation.getCurrentPosition(position => {
         const {latitude, longitude} = position.coords;
-
-        console.log(position);
-
-        // HTML에서 체크된 카테고리를 가져와서 categoryList 배열에 추가
         const categoryList = [];
+
+        // 체크된 카테고리 가져오기
+        const koreanCheckbox = document.getElementById("Korean");
+        const japaneseCheckbox = document.getElementById("Japanese");
+        const chineseCheckbox = document.getElementById("Chinese");
+        const westernCheckbox = document.getElementById("Western");
+
         if (document.getElementById("Korean").checked) {
             categoryList.push("KOREAN");
         }
@@ -80,31 +97,34 @@ function recommend() {
                 if (!response.ok) {
                     throw new Error('Network response was not ok');
                 }
-
                 return response.json();
             })
             .then(data => {
-                console.log('서버 응답:', data);
-
                 if (data && data.recommendedRestaurants && data.recommendedRestaurants.length > 0) {
-                    let listElement = $('ol.list-group');
-                    listElement.empty();
+                    const listElement = document.querySelector('ol.list-group');
+                    listElement.innerHTML = ''; // 리스트 초기화
 
                     data.recommendedRestaurants.forEach(function (item) {
-                        listElement.append(`
-                            <li class="list-group-item">
-                                <div class="ms-2 me-auto">
-                                    <div class="fw-bold">${item.category}</div>
-                                    <div>${item.streetNameAddress}</div>
-                                    <div>${item.restaurantName}</div>
-                                </div>
-                            </li>
-                        `);
+                        const listItem = document.createElement('li');
+                        listItem.classList.add('list-group-item');
+                        listItem.innerHTML = `
+                        <div class="ms-2 me-auto">
+                            <div class="fw-bold">${item.category}</div>
+                            <div>${item.streetNameAddress}</div>
+                            <div>${item.restaurantName}</div>
+                        </div>
+                    `;
+                        listElement.appendChild(listItem);
                     });
                 } else {
-                    // Handle case when no data or empty array received
-                    $('.alert.alert-warning').show();
-                    $('ol.list-group').empty();
+                    const alertWarning = document.querySelector('.alert.alert-warning');
+                    if (alertWarning) {
+                        alertWarning.style.display = 'block';
+                    }
+                    const listGroup = document.querySelector('ol.list-group');
+                    if (listGroup) {
+                        listGroup.innerHTML = '';
+                    }
                 }
             })
             .catch(error => {
