fda97aa72391ce275993983a267f7838309e980a
daramCoder <tjsdkyd4027@gmail.com>
2024-05-23 16:01:47 +0900

    주석 제거

--- src/main/java/toy/ojm/domain/location/TransCoordination.java
+++ src/main/java/toy/ojm/domain/location/TransCoordination.java
@@ -35,33 +35,6 @@ public class TransCoordination {
         // 좌표 변환 수행
         transformer.transform(beforeCoord, afterCoord);
 
-
-
-//        // CRS 객체 생성
-//        CRSFactory factory = new CRSFactory();  // CoordinateReferenceSystem을 생성하기 위한 CRSFactory 인스턴스 생성
-//
-//        // 중부원점 좌표계 정의
-//        String utmkName = "UTMK";
-//        String utmkProj = "+proj=tmerc +lat_0=38 +lon_0=127 +k=1 +x_0=200000 +y_0=500000 +ellps=bessel +units=m +no_defs";
-//        CoordinateReferenceSystem utmkSystem = factory.createFromParameters(utmkName, utmkProj);
-//
-//        // WGS84 좌표계 정의
-//        String wgs84Name = "WGS84";
-//        String wgs84Proj = "+proj=longlat +datum=WGS84 +no_defs";
-//        CoordinateReferenceSystem wgs84System = factory.createFromParameters(wgs84Name, wgs84Proj);
-//
-//        CoordinateTransformFactory ctFactory = new CoordinateTransformFactory();
-//        // 변환 시스템 지정. (원본 시스템, 변환 시스템)
-//        CoordinateTransform coordinateTransform = ctFactory.createTransform(utmkSystem, wgs84System);
-//
-//        // 변환할 좌표계 정보 생성
-//        ProjCoordinate beforeCoord = new ProjCoordinate(x, y);
-//        // 변환된 좌표를 담을 객체 생성
-//        ProjCoordinate afterCoord = new ProjCoordinate();
-//
-//        // 좌표 변환
-//        coordinateTransform.transform(beforeCoord, afterCoord);
-
         // 변환된 좌표 로그 출력
         log.info("변환된 경도 : " + afterCoord.x);
         log.info("변환된 위도 : " + afterCoord.y);
