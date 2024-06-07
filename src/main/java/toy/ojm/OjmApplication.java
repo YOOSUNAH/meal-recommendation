cc06b1bbf39b85335cccc659b2dba5c7c7d43d60
daramCoder <tjsdkyd4027@gmail.com>
2024-06-07 22:40:31 +0900

    운영서버 테스트용 로그

--- src/main/java/toy/ojm/OjmApplication.java
+++ src/main/java/toy/ojm/OjmApplication.java
@@ -1,13 +1,16 @@
 package toy.ojm;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-
+@Slf4j
 public class OjmApplication {
 
     public static void main(String[] args) {
+
+        log.info("시작 하기 전에 로그 남기기 ");
         SpringApplication.run(OjmApplication.class, args);
 
     }
