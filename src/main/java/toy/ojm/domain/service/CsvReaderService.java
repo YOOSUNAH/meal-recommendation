cf5e05b9a2c8c97420551e22f243f1297ad5279e
daramCoder <tjsdkyd4027@gmail.com>
2024-06-13 15:14:00 +0900

    CSV 크롤링 -> DB 저장 로직 리팩토링

--- src/main/java/toy/ojm/domain/service/CsvReaderService.java
+++ src/main/java/toy/ojm/domain/service/CsvReaderService.java
@@ -4,16 +4,21 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 import toy.ojm.domain.entity.Restaurant;
 import toy.ojm.domain.location.TransCoordination;
 import toy.ojm.domain.repository.RestaurantRepository;
 import org.locationtech.proj4j.ProjCoordinate;
+import toy.ojm.infrastructure.PublicDataConstants;
 
 @Slf4j
 @Service
@@ -25,9 +30,10 @@ public class CsvReaderService {
 
     public void readAndSaveCSV() {
         try {
-            File file = new File("out/production/resources/csv-data/data.csv");
+            Path csvFilePath = Paths.get(new ClassPathResource(PublicDataConstants.DESTINATION_DIRECTORY).getFile().getAbsolutePath()).resolve(PublicDataConstants.DESTINATION_FILE_NAME + "." + PublicDataConstants.DESTINATION_FILE_EXTENSION);
+            File file = new File(csvFilePath.toString());
             BufferedReader br = new BufferedReader(
-                new InputStreamReader(new FileInputStream(file), "EUC-KR"));
+                new InputStreamReader(new FileInputStream(file), Charset.forName("EUC_KR")));
             String line;
 
             // 첫 번째 행(제목 행)을 읽고 버림
@@ -40,45 +46,55 @@ public class CsvReaderService {
                 String[] lineArr = line.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)", -1);
                 aLine = Arrays.asList(lineArr);
 
+                String businessStatus = aLine.get(7);
+                if (businessStatus != null && businessStatus.contains("폐업")) {
+                    continue;
+                }
+
                 Restaurant restaurant = new Restaurant();
-                restaurant.setBusinessStatus(aLine.get(7));
-                restaurant.setNumber(aLine.get(12));
-                restaurant.setAddress(aLine.get(15));
-                restaurant.setRoadAddress(aLine.get(16));
-                restaurant.setName(aLine.get(18));
-                restaurant.setCategory(aLine.get(22));
+                restaurant.setBusinessStatus(removeDoubleQuote(aLine.get(7)));
+                restaurant.setNumber(removeDoubleQuote(aLine.get(12)));
+                restaurant.setAddress(removeDoubleQuote(aLine.get(15)));
+                restaurant.setRoadAddress(removeDoubleQuote(aLine.get(16)));
+                restaurant.setName(removeDoubleQuote(aLine.get(18)));
+                restaurant.setCategory(removeDoubleQuote(aLine.get(22)));
 
                 String beforeLongitude =  aLine.get(23);
                 String beforeLatitude =  aLine.get(24);
 
-                try {
-                    restaurant.setLongitude(Double.parseDouble(beforeLongitude.replaceAll("\"", "")));  // 쌍따옴표제거 추가
-                } catch (NumberFormatException e) {
-                    log.error("Invalid number format for longitude: " + aLine.get(23));
-                    restaurant.setLongitude(0.0);
-                }
-
-                try {
-                    restaurant.setLatitude(Double.parseDouble(beforeLatitude.replaceAll("\"", "")));
-                } catch (NumberFormatException e) {
-                    log.error("Invalid number format for latitude: " + aLine.get(24));
-                    restaurant.setLatitude(0.0);
-                }
+                restaurant.setLongitude(parseDouble(beforeLongitude, aLine));
+                restaurant.setLatitude(parseDouble(beforeLatitude, aLine));
 
                 // 좌표 변경
-                 ProjCoordinate coordinate = transCoordination.transformToWGS(
+                 ProjCoordinate transformed = transCoordination.transformToWGS(
                 restaurant.getLongitude(), restaurant.getLatitude());
-                restaurant.setLongitude(coordinate.x);
-                restaurant.setLatitude(coordinate.y);
+                restaurant.setLongitude(transformed.x);
+                restaurant.setLatitude(transformed.y);
 
                 restaurants.add(restaurant);
-
-                restaurantRepository.saveAll(restaurants);
             }
-
+            restaurantRepository.saveAll(restaurants);
         } catch (Exception e) {
             log.error(e.getMessage());
         }
+    }
+
+    private double parseDouble(String coordinate, List<String> aLine){
+        try{
+            return Double.parseDouble(removeDoubleQuote(coordinate));
+        }
+        catch(NumberFormatException e){
+            log.error("Invalid number format for coordinate. {} - {}", aLine.get(18), aLine.get(23));
+        }
+        return 0;
+    }
+
+    // "" 제거 하기
+    private String removeDoubleQuote(String value){
+        if(value == null || value.isBlank()){
+            return "";
+        }
+        return value.replaceAll("\"", "");
     }
 
     public List<Restaurant> getAllRestaurants() {
