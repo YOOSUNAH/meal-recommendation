a37a4abb06edb7ad4344f74c8d98cc5838a0a46d
daramCoder <tjsdkyd4027@gmail.com>
2024-06-13 15:48:43 +0900

    refactor

--- src/main/java/toy/ojm/domain/service/CsvReaderService.java
+++ src/main/java/toy/ojm/domain/service/CsvReaderService.java
@@ -3,6 +3,7 @@ package toy.ojm.domain.service;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Path;
@@ -29,11 +30,21 @@ public class CsvReaderService {
     private final TransCoordination transCoordination;
 
     public void readAndSaveCSV() {
+        Path csvFilePath;
+        File csvFile;
+        BufferedReader br = null;
+        InputStreamReader isr = null;
+        FileInputStream fis = null;
+
         try {
-            Path csvFilePath = Paths.get(new ClassPathResource(PublicDataConstants.DESTINATION_DIRECTORY).getFile().getAbsolutePath()).resolve(PublicDataConstants.DESTINATION_FILE_NAME + "." + PublicDataConstants.DESTINATION_FILE_EXTENSION);
-            File file = new File(csvFilePath.toString());
-            BufferedReader br = new BufferedReader(
-                new InputStreamReader(new FileInputStream(file), Charset.forName("EUC_KR")));
+            csvFilePath = Paths.get(
+                new ClassPathResource(PublicDataConstants.DESTINATION_DIRECTORY).getFile()
+                    .getAbsolutePath()).resolve(PublicDataConstants.DESTINATION_FILE_NAME + "."
+                + PublicDataConstants.DESTINATION_FILE_EXTENSION);
+            csvFile = new File(csvFilePath.toString());
+            fis = new FileInputStream(csvFile);
+            isr = new InputStreamReader(fis, Charset.forName("EUC_KR"));
+            br = new BufferedReader(isr);
             String line;
 
             // 첫 번째 행(제목 행)을 읽고 버림
@@ -42,9 +53,8 @@ public class CsvReaderService {
             List<Restaurant> restaurants = new ArrayList<>();
 
             while ((line = br.readLine()) != null) {
-                List<String> aLine = new ArrayList<>();
-                String[] lineArr = line.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)", -1);
-                aLine = Arrays.asList(lineArr);
+                List<String> aLine = Arrays.asList(
+                    line.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)", -1));
 
                 String businessStatus = aLine.get(7);
                 if (businessStatus != null && businessStatus.contains("폐업")) {
@@ -59,15 +69,15 @@ public class CsvReaderService {
                 restaurant.setName(removeDoubleQuote(aLine.get(18)));
                 restaurant.setCategory(removeDoubleQuote(aLine.get(22)));
 
-                String beforeLongitude =  aLine.get(23);
-                String beforeLatitude =  aLine.get(24);
+                String beforeLongitude = aLine.get(23);
+                String beforeLatitude = aLine.get(24);
 
                 restaurant.setLongitude(parseDouble(beforeLongitude, aLine));
                 restaurant.setLatitude(parseDouble(beforeLatitude, aLine));
 
                 // 좌표 변경
-                 ProjCoordinate transformed = transCoordination.transformToWGS(
-                restaurant.getLongitude(), restaurant.getLatitude());
+                ProjCoordinate transformed = transCoordination.transformToWGS(
+                    restaurant.getLongitude(), restaurant.getLatitude());
                 restaurant.setLongitude(transformed.x);
                 restaurant.setLatitude(transformed.y);
 
@@ -76,22 +86,44 @@ public class CsvReaderService {
             restaurantRepository.saveAll(restaurants);
         } catch (Exception e) {
             log.error(e.getMessage());
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException ignore) {
+
+                }
+            }
+            if (isr != null) {
+                try {
+                    isr.close();
+                } catch (IOException ignore) {
+
+                }
+            }
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException ignore) {
+
+                }
+            }
         }
     }
 
-    private double parseDouble(String coordinate, List<String> aLine){
-        try{
+    private double parseDouble(String coordinate, List<String> aLine) {
+        try {
             return Double.parseDouble(removeDoubleQuote(coordinate));
-        }
-        catch(NumberFormatException e){
-            log.error("Invalid number format for coordinate. {} - {}", aLine.get(18), aLine.get(23));
+        } catch (NumberFormatException e) {
+            log.error("Invalid number format for coordinate. {} - {}", aLine.get(18),
+                aLine.get(23));
         }
         return 0;
     }
 
     // "" 제거 하기
-    private String removeDoubleQuote(String value){
-        if(value == null || value.isBlank()){
+    private String removeDoubleQuote(String value) {
+        if (value == null || value.isBlank()) {
             return "";
         }
         return value.replaceAll("\"", "");
