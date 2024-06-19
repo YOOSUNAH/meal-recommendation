0b175e557be30f673295bdf2ea482a46b35e410b
daramCoder <tjsdkyd4027@gmail.com>
2024-06-19 22:26:30 +0900

    build를 위해 수정

--- src/main/java/toy/ojm/global/utils/ExceptionUtils.java
+++ src/main/java/toy/ojm/global/utils/ExceptionUtils.java
@@ -1,27 +1,28 @@
-package toy.ojm.global.utils;
-
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
-
-import java.util.function.Consumer;
-
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
-public class ExceptionUtils {
-
-    public static <E extends Exception> void handleException(
-        Runnable successAction,
-        Consumer<E> failAction
-    ) {
-        try {
-            successAction.run();
-        } catch (Exception e) {
-            try {
-                E ex = (E) e;
-                failAction.accept(ex);
-            } catch (ClassCastException ignored) {
-            }
-        }
-    }
-
-
-}
+//package toy.ojm.global.utils;
+//
+//import lombok.AccessLevel;
+//import lombok.NoArgsConstructor;
+//
+//import java.util.function.Consumer;
+//
+//@NoArgsConstructor(access = AccessLevel.PRIVATE)
+//public class ExceptionUtils {
+//
+//    public static <E extends Exception> void handleException(
+//        Runnable successAction,
+//        Consumer<E> failAction
+//    ) {
+//        try {
+//            successAction.run();
+//        } catch (Exception e) {
+//            try {
+//                @SuppressWarnings("unchecked")
+//                E ex = (E) e;
+//                failAction.accept(ex);
+//            } catch (ClassCastException ignored) {
+//            }
+//        }
+//    }
+//
+//
+//}
