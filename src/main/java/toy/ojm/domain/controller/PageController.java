b5e04697a807861a86cbd8af941669ebaa96c52b
daramCoder <tjsdkyd4027@gmail.com>
2024-07-02 23:29:42 +0900

    제공되지 않는 페이지 요청 시 홈으로 리다이렉트

--- src/main/java/toy/ojm/domain/controller/PageController.java
+++ src/main/java/toy/ojm/domain/controller/PageController.java
@@ -13,75 +13,24 @@ public class PageController {
     private String kakaoAppKey;
 
     @GetMapping("/")
+    public String homepage() {
+        return "home";
+    }
+
+    @GetMapping("/home")
     public String home() {
-        return "recommend";
+        return "home";
     }
-
-    @GetMapping("/pageForm1")
-    public String pageForm() {
-        return "fragments/page-form-1";
-    }
-
-    @GetMapping("/pageForm2")
-    public String pageForm2() {
-        return "fragments/page-form-2";
-    }
-
-    @GetMapping("/pageForm3")
-    public String pageForm3() {
-        return "fragments/page-form-3";
-    }
-
-    @GetMapping("/page1")
-    public String page() {
-        return "page1";
-    }
-
-    @GetMapping("/page2")
-    public String page2(Model model) {
-        model.addAttribute("kakaoAppKey", kakaoAppKey);
-        return "page2";
-    }
-
-    @GetMapping("/page3")
-    public String page3() {
-        return "page3";
-    }
-
-    @GetMapping("/page4")
-    public String page4() {
-        return "page4";
-    }
-
 
     @GetMapping("/location")
-    public String location() {
+    public String location(Model model) {
+        model.addAttribute("kakaoAppKey", kakaoAppKey);
         return "location";
     }
 
-    @GetMapping("/map0")
-    public String map0() {
-        return "map/map0";
-    }
-
-    @GetMapping("/map1")
-    public String map2() {
-        return "map/map1";
-    }
-
-    @GetMapping("/map3")
-    public String map3() {
-        return "map/map3";
-    }
-
-    @GetMapping("/map4")
-    public String map4() {
-        return "map/map4";
-    }
-
-    @GetMapping("/locationSample")
-    public String locationSample() {
-        return "map/geolocationSample";
+    @GetMapping("/category")
+    public String category() {
+        return "category";
     }
 
     @GetMapping("/result")
@@ -89,18 +38,6 @@ public class PageController {
         return "result";
     }
 
-    @GetMapping("/result2")
-    public String result2() {
-        return "result2";
-    }
 
-    @GetMapping("/result3")
-    public String result3() {
-        return "result3";
-    }
 
-    @GetMapping("/recommendation")
-    public String recommendation() {
-        return "result2";
-    }
 }
