ecec9474def2fab64939bd0cef91cd8023f236f1
daramCoder <tjsdkyd4027@gmail.com>
2024-07-03 15:36:57 +0900

    제공되지 않는 페이지 요청 시 홈으로 리다이렉트 (엔드포인트가 잘못되었을 경우) , 사용되지 않는 파일 삭제

--- src/main/java/toy/ojm/domain/controller/PageController.java
+++ src/main/java/toy/ojm/domain/controller/PageController.java
@@ -4,8 +4,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 @Controller
@@ -40,10 +44,9 @@ public class PageController implements ErrorController {
         return "result";
     }
 
-
     @RequestMapping("/error")
     public String handleError() {
-        return "home";
+        return "redirect:/home";
     }
 
     public String getErrorPath() {
