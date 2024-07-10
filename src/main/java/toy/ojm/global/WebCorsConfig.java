ab328c17648cafad041945ff20a7789fcedac0f7
daramCoder <tjsdkyd4027@gmail.com>
2024-07-10 16:59:25 +0900

    Mixed content 문제 해결중(Config를 통해)

---
+++ src/main/java/toy/ojm/global/WebCorsConfig.java
@@ -0,0 +1,19 @@
+package toy.ojm.global;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebCorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOriginPatterns("*") // 패턴을 사용하여 허용할 오리진 설정
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .exposedHeaders("Authorization")
+            .allowCredentials(true);
+    }
+}
