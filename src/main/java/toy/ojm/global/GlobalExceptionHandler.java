6f34cd35048530e74f5ebf3bbc3f876fcbbcc015
e-build <donggeol2@kakao.com>
2024-07-17 21:50:22 +0900

    renewal ver 1

--- src/main/java/toy/ojm/global/GlobalExceptionHandler.java
+++ src/main/java/toy/ojm/global/GlobalExceptionHandler.java
@@ -9,38 +9,38 @@ import org.springframework.web.servlet.NoHandlerFoundException;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(NoHandlerFoundException.class)
-    public String handle404(NoHandlerFoundException exception) {
-        String requestURL = exception.getRequestURL();
-
-        // 무한 루프를 방지
-        if ("/home".equals(requestURL)) {
-            return "home";
-        }
-
-        if (requestURL.contains("login")) {
-            log.debug("# exception 의 URL " + requestURL);
-            return "login";
-
-        } else if (requestURL.contains("/boss")) {
-            return "login";
-
-        } else if (requestURL.contains("/adminPage")) {
-            return "adminPage";
-
-        } else if (requestURL.contains("admin/get-session")) {
-            return "forward:/v1/admin/get-session";
-
-        } else if (requestURL.contains("admin/encodePassword")) {
-            return "forward:/v1/admin/encodePassword";
-
-        } else if (requestURL.contains("admin/restaurants")) {
-            return "forward:/v1/admin/restaurants";
-
-        } else if (requestURL.startsWith("/css") || requestURL.startsWith("/js")) {
-            return "forward:" + requestURL;
-        }
-
-        return "redirect:/home";
-    }
+//    @ExceptionHandler(NoHandlerFoundException.class)
+//    public String handle404(NoHandlerFoundException exception) {
+//        String requestURL = exception.getRequestURL();
+//
+//        // 무한 루프를 방지
+//        if ("/home".equals(requestURL)) {
+//            return "home";
+//        }
+//
+//        if (requestURL.contains("login")) {
+//            log.debug("# exception 의 URL " + requestURL);
+//            return "login";
+//
+//        } else if (requestURL.contains("/boss")) {
+//            return "login";
+//
+//        } else if (requestURL.contains("/adminPage")) {
+//            return "adminPage";
+//
+//        } else if (requestURL.contains("admin/get-session")) {
+//            return "forward:/v1/admin/get-session";
+//
+//        } else if (requestURL.contains("admin/encodePassword")) {
+//            return "forward:/v1/admin/encodePassword";
+//
+//        } else if (requestURL.contains("admin/restaurants")) {
+//            return "forward:/v1/admin/restaurants";
+//
+//        } else if (requestURL.startsWith("/css") || requestURL.startsWith("/js")) {
+//            return "forward:" + requestURL;
+//        }
+//
+//        return "redirect:/home";
+//    }
 }
