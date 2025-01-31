3f7e8af44766dd0203b63560351403848af99598
YOOSUNAH <tjsdkyd4027@gmail.com>
2025-01-31 21:17:41 +0900

    사용되지 않는 부분 삭제

--- src/main/java/toy/ojm/domain/repository/RestaurantRepository.java
+++ src/main/java/toy/ojm/domain/repository/RestaurantRepository.java
@@ -13,8 +13,5 @@ public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
 
     List<Restaurant> findAllByCategoryIn(List<String> categories);
 
-    Page<Restaurant> findAll(Specification<Restaurant> specification, Pageable pageable);
-
-    Optional<Restaurant> findByManagementNumber(String managementNumber);
 }
 
