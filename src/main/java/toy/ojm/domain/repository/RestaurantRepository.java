19f65384423cf8832e1ebd6753e3f8753e296118
YOOSUNAH <tjsdkyd4027@gmail.com>
2025-02-05 19:07:43 +0900

    비동기 처리 - Thread Pool 활용 - 불필요한 부분 제거

--- src/main/java/toy/ojm/domain/repository/RestaurantRepository.java
+++ src/main/java/toy/ojm/domain/repository/RestaurantRepository.java
@@ -1,7 +1,6 @@
 package toy.ojm.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 import toy.ojm.domain.entity.Restaurant;
 
 import java.util.List;
@@ -10,7 +9,7 @@ public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
 
     List<Restaurant> findAllByCategoryIn(List<String> categories);
 
-    List<Restaurant> findAllByManagementNumberIn(@Param("numbers") List<String> managementNumbers);
+    List<Restaurant> findAllByManagementNumberIn(List<String> managementNumbers);
 
 }
 
